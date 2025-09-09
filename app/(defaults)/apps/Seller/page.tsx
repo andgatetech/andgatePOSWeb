29c297ae638d3316455fe6237cfb94b858931971
M. Abir Alam <malam19197@bscse.uiu.ac.bd>
2025-09-09 17:49:19 +0600

    seller page fixed

---
+++ app/(defaults)/apps/Seller/page.tsx
@@ -0,0 +1,8 @@
+import ComponentsPagesComingSoonForm from "@/components/pages/coming-soon/components-pages-coming-soon-form";
+
+
+const page = () => {
+    return <div className="  "><ComponentsPagesComingSoonForm></ComponentsPagesComingSoonForm></div>;
+};
+
+export default page;
