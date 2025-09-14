bf07a64f19f8e54c1923e7adc6c61596439bdc33
meherab151175 <meherabm803@gmail.com>
2025-09-14 12:50:04 +0600

    config changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import 'react-perfect-scrollbar/dist/css/styles.css';
 import '../styles/tailwind.css';
 import { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
+import WhatsAppButton from '@/__components/WhatsAppButton';
 
 export const metadata: Metadata = {
     title: {
@@ -22,7 +23,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en">
             <body className={nunito.variable}>
-                <ProviderComponent>{children}</ProviderComponent>
+                <ProviderComponent>
+                    {children}
+                    <WhatsAppButton />
+                </ProviderComponent>
             </body>
         </html>
     );
