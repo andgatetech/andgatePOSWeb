bbb58c4f66a2d3a3f582ad0c40a2333f675603d2
M. Abir Alam <malam191197@bscse.uiu.ac.bd>
2025-11-23 13:28:53 +0600

    fix: design and icon

--- app/(defaults)/(apps)/products/stock/adjustments/page.tsx
+++ app/(defaults)/(apps)/products/stock/adjustments/page.tsx
@@ -1,9 +1,17 @@
+import { Box, Package } from 'lucide-react';
 import PosLeftSide from '../../../pos/PosLeftSide';
 import StockAdjustment from './component/StockAdjustment';
 
 const page = () => {
     return (
-        <PosLeftSide disableSerialSelection={true}>
+        <PosLeftSide
+            disableSerialSelection={true}
+            mobileButtonConfig={{
+                showIcon: <Box className="h-6 w-6" />,
+                hideIcon: <Package className="h-6 w-6" />,
+                label: 'Stock',
+            }}
+        >
             <StockAdjustment></StockAdjustment>
         </PosLeftSide>
     );
