8eb6dcf3f189fc30546782e9f42d94c5676f3106
Khorshed Alam <andgatetech@gmail.com>
2026-07-05 12:39:05 +0600

    pixel and google tag implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -210,34 +210,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
                         `}
                     </Script>
                 )}
-                {/* <Script
-                id="facebook-pixel"
-                strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    !function(f,b,e,v,n,t,s)
-                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                    n.queue=[];t=b.createElement(e);t.async=!0;
-                    t.src=v;s=b.getElementsByTagName(e)[0];
-                    s.parentNode.insertBefore(t,s)}(window, document,'script',
-                    'https://connect.facebook.net/en_US/fbevents.js');
-                    fbq('init', '1803836553585330');
-                    fbq('track', 'PageView');
-                    `,
-                }}
-                />
-
-                <noscript>
-                <img
-                    height="1"
-                    width="1"
-                    style={{ display: 'none' }}
-                    src="https://www.facebook.com/tr?id=1803836553585330&ev=PageView&noscript=1"
-                    alt="facebook pixel"
-                />
-                </noscript> */}
             </head>
             <body suppressHydrationWarning>
                 {/* GTM noscript fallback — required by Google, goes right after <body> */}
@@ -304,6 +276,17 @@ export default async function RootLayout({ children }: { children: React.ReactNo
                             fbq('track', 'PageView');
                         `}
                     </Script>
+                )}
+                {process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID && (
+                    <noscript>
+                        <img
+                            height="1"
+                            width="1"
+                            style={{ display: 'none' }}
+                            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
+                            alt=""
+                        />
+                    </noscript>
                 )}
             </body>
         </html>
